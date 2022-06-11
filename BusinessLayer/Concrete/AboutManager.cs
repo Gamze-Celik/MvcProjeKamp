2302b7fe74a90b6faab3fab9d2384e79727beaa8
Gamze Çelik <79765550+Gamze-Celik@users.noreply.github.com>
2022-06-11 17:10:41 +0300

    Add files via upload

---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+
+namespace Business.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboutDal;
+
+        public AboutManager(IAboutDal aboutDal)
+        {
+            _aboutDal = aboutDal;
+        }
+
+        public void AboutAdd(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutDelete(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AboutUpdate(About about)
+        {
+            throw new NotImplementedException();
+        }
+
+        public About GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> GetListByHeadingID(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
