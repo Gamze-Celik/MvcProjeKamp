9a6c2dcf60ccdcb77935bcae239853b2087d29b8
Gamze Çelik <79765550+Gamze-Celik@users.noreply.github.com>
2022-06-11 17:09:08 +0300

    Add files via upload

---
+++ BusinessLayer/Abstract/IAboutService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Entity.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IAboutService
+    {
+        List<About> GetList();
+        List<About> GetListByHeadingID(int id);
+        void AboutAdd(About about);
+        About GetByID(int id);
+        void AboutDelete(About about);
+        void AboutUpdate(About about);
+    }
+}
---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Entity.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        List<Content> GetListByHeadingID(int id);
+        void ContentAdd(Content content);
+        Content GetByID(int id);
+        void ContentDelete(Content content);
+        void ContentUpdate(Content content);
+    }
+}
